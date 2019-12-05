433c5963c3f67d386dd737d0d5c8e311534dbbb9
Kobe Kupp <kobethomas35@yahoo.com>
2019-12-05 13:52:18 -0800

    Added Room, RoomFactory, RoomContent, and Door

--- DungeonAdventure/src/Dungeon.java
+++ DungeonAdventure/src/Dungeon.java
@@ -1,137 +1,132 @@
 
 
 
+
+
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Random;
 
-/**
+public class Dungeon {
+	
+	private Room entrance;
+	private Room exit;
+	
+	
+	
+	
+	
+	
+	
+	public Object getEntrance() {
+		return entrance;
+	}
 
- * Title: Dungeon.java
- *
- * Description: Driver file for Heroes and Monsters project
- *
- * Copyright:    Copyright (c) 2001
- * Company: Code Dogs Inc.
- * I.M. Knurdy
- *
- * History:
- *  11/4/2001: Wrote program
- *    --created DungeonCharacter class
- *    --created Hero class
- *    --created Monster class
- *    --had Hero battle Monster
- *    --fixed attack quirks (dead monster can no longer attack)
- *    --made Hero and Monster abstract
- *    --created Warrior
- *    --created Ogre
- *    --made Warrior and Ogre battle
- *    --added battleChoices to Hero
- *    --added special skill to Warrior
- *    --made Warrior and Ogre battle
- *    --created Sorceress
- *    --created Thief
- *    --created Skeleton
- *    --created Gremlin
- *    --added game play features to Dungeon.java (this file)
- *  11/27/2001: Finished documenting program
- * version 1.0
- */
-
-
-
-/*
-  This class is the driver file for the Heroes and Monsters project.  It will
-  do the following:
-
-  1.  Allow the user to choose a hero
-  2.  Randomly select a monster
-  3.  Allow the hero to battle the monster
-
-  Once a battle concludes, the user has the option of repeating the above
-
-*/
-public class Dungeon
-{
-    public static void main(String[] args)
-	{
-     HeroFactory factory = new HeroFactory();
-     MonsterFactory monster = new MonsterFactory();
-		Hero theHero;
-		Monster theMonster;
-
+	public void setEntrance() {
 		
-		Room.class.toString();
-		
-		do
-		{
-			theHero = factory.createHero();
-		    theMonster = monster.generateMonster();
-			battle(theHero, theMonster);
+	}
 
-		} while (playAgain());
+	public Object getExit() {
+		return exit;
+	}
 
-    }//end main method
+	public void setExit(int[][] exit) {
+		this.exit = exit;
+	}
+
+	
+
+	public Dungeon(Room[][] dungeon) {
+		//dungeon.createDungeon();
+		 dungeon = new Room[5][5];
+    	
+	}
+	
+	
+	
+	public static void createDungeon() {
+    	Room[][] dungeon = new Room[5][5];
+    	dungeon[0][0] = new Room();
     
-    
-   
-   
-
-	 	
-	public static boolean playAgain()
-	{
-		char again;
-		Scanner kin = new Scanner(System.in);
-
-		System.out.println("Play again (y/n)?");
-		again = kin.next().charAt(0);
+    }
+	
+	public void setSpawn(Room[][] entrance) {
+		//this is to make sure that the entrance is placed on the border
+		//of the dungeon, and not in the middle.
+		int border = new Random().nextInt(5);
+		int randoEntry = new Random().nextInt(6);
+		//int randoExit = new Random().nextInt(6);
 		
-		if(again == 'Y' || again == 'y') {
-			return true;
+		
+		
+		if(border == 0) {
+			entrance[0][randoEntry].getEntrance();
 		}
-	 
-		return false;
-	}//end playAgain method
-
-
-/*-------------------------------------------------------------------
-battle is the actual combat portion of the game.  It requires a Hero
-and a Monster to be passed in.  Battle occurs in rounds.  The Hero
-goes first, then the Monster.  At the conclusion of each round, the
-user has the option of quitting.
----------------------------------------------------------------------*/
-	public static void battle(Hero theHero, Monster theMonster)
-	{
+		else if(border == 1) {
+			entrance[randoEntry][0].getEntrance();
+		}
+		else if(border == 2) {
+			entrance[5][randoEntry].getEntrance();
+		}
+		else {
+			entrance[randoEntry][5].getEntrance();
+		}
 		
-		Scanner kb = new Scanner(System.in);
-		char pause = 'p';
-		System.out.println(theHero.getName() + " battles " +
-							theMonster.getName());
-		System.out.println("---------------------------------------------");
+	}
+	
+	public void setExitDoor(Room[][] exit) {
+		int row = new Random().nextInt(6);
+		int col = new Random().nextInt(6);
+		
+		if(!exit[row][col].hasEntrance()) {
+			exit[row][col].getExit();
+		}
+		else {
+			row = new Random().nextInt(6);
+			col = new Random().nextInt(6);
+			exit[row][col].getExit();
+		}
+	}
+	
+	public void placePillar(Room[][] room) {
+		int row = new Random().nextInt(6);
+		int col = new Random().nextInt(6);
+		int count = 0;
+		
+		while(count != 4) {
+			if(room[row][col].hasPillar() || room[row][col].hasEntrance() || room[row][col].hasExit()) {
+				row = new Random().nextInt(6);
+				col = new Random().nextInt(6);
+			}
+			room[row][col].getPillar();
+			count++;
+		}
+		
+	}
+	
+	
+	  public boolean hasEntrance() { 
+		  if(this.getEntrance() != null) {
+			  return true;
+		  } 
+		  return false;
+	  }
+	  
+	  public boolean hasExit() {
+		  if(this.getExit() != null) {
+			  return true; 
+			  } 
+		  return false;
+	  }
+	
+	
+	
+	public String toString() {
+		Room[][] array = new Room[5][5];
+		System.out.println(Arrays.deepToString(array));
+		
+		return "test";
+	}
 
-		//do battle
-		while (theHero.isAlive() && theMonster.isAlive() && pause != 'q')
-		{
-		    //hero goes first
-			theHero.battleChoices(theMonster);
+}
 
-			//monster's turn (provided it's still alive!)
-			if (theMonster.isAlive())
-			    theMonster.attack(theHero);
-
-			//let the player bail out if desired
-			System.out.print("\n-->q to quit, anything else to continue: ");
-			pause = kb.next().charAt(0);
-
-		}//end battle loop
-
-		if (!theMonster.isAlive())
-		    System.out.println(theHero.getName() + " was victorious!");
-		else if (!theHero.isAlive())
-			System.out.println(theHero.getName() + " was defeated :-(");
-		else//both are alive so user quit the game
-			System.out.println("Quitters never win ;-)");
-
-	}//end battle method
-
-
-}//end Dungeon class
+    
